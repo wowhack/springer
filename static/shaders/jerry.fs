44b473bd113e9469ae333876f2bf4b84449ba60b
jhonny@laptop <jhonny@laptop>
2014-08-06 15:12:26 +0200

    Cool alpha time

--- static/shaders/jerry.fs
+++ static/shaders/jerry.fs
@@ -4,11 +4,18 @@ uniform sampler2D tex0;
 uniform float vis;
 uniform vec2 tex_size;
 
+uniform float alpha;
+
 varying vec2 v_uv0;
 
 void main()
 {
 	// gl_FragColor = vec4( v_uv0, 0.0, 1.0 );
 	// gl_FragColor = vec4( v_uv0, 0.0, 1.0 );
-	gl_FragColor = texture2D( tex0, v_uv0 );
+
+	vec4 sample = texture2D( tex0, v_uv0 );
+
+
+
+	gl_FragColor = sample * alpha;
 }
